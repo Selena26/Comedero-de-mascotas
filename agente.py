48e9d037b77486673ff48930d984b53e958c6c17
Selena26 <75510255+Selena26@users.noreply.github.com>
2022-05-25 17:27:47 -0500

    Funcìón para el comedero de mascotas-inicializar el estado objetivo y el costo

---
+++ agente.py
@@ -0,0 +1,6 @@
+#Creación de la función para el funcionamiento del agente autónomo
+def comedero_mascota();
+    #Inicializar el estado_meta
+    #Lleno=0; Vacio=1
+    estado_meta={'7am': '0', "12pm": '0', '7pm': '0'}
+    costo=0 #El costo inicializa en 0
