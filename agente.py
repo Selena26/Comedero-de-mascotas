7d04a8a164c7d0cf4984026b2d87d0b02f8c318b
Selena26 <75510255+Selena26@users.noreply.github.com>
2022-05-25 21:01:02 -0500

    Verificacion de la primera locacion si el estado esta en 1

--- agente.py
+++ agente.py
@@ -9,6 +9,41 @@ def comedero_mascota():
     ingrese_estado = input("Ingrese el estado del comedero para mascotas a las: "+ingrese_hora+" ")#El usuario ingresa el estado del comedero: lleno o vacio
 
     if (ingrese_hora == '7am'):#El usuario ingresa las horas programadas
-        ingrese_hora2 = input("Ingrese el estado de la hora programada: ")#El usuario ingresa el estado del comedero: lleno o vacio
-        ingrese_hora3 = input("Ingrese el estado de la hora programada: ")#El usuario ingresa el estado del comedero: lleno o vacio
-        print("Meta Deseada:" + str(estado_meta))#Se muestran los resultados
+        estado_hora2 = input("Ingrese el estado de la hora programada: ")#El usuario ingresa el estado del comedero: lleno o vacio
+        estado_hora3 = input("Ingrese el estado de la hora programada: ")#El usuario ingresa el estado del comedero: lleno o vacio
+        print("Meta Deseada:" + str(estado_meta))#Se muestran los resultados que se esperan
+        #Muestra el horario en el que se encuentra el comedero para mascotas
+        print("El comedero para mascotas se encuentra en el horario 7am")
+        if(ingrese_estado == '1'):#Si el comedero para mascotas a las 7am se encuentra vacio
+            print("El comedero para mascotas a las 7am esta vacio")#Muestra mensaje de que el comedero para mascotas esta vacio a las 7am
+            estado_meta['7am'] ='0' #El comedero de mascotas debe llenarse
+            costo += 1 #El costo aumentara cada vez que el comedero cambie de estado, es decir de estar vacio pase a estar lleno
+            print("El comedero para mascotas se esta llenando a las 7am")#Muestra que el comedero para mascotas se esta llenando a esa hora
+            print("El costo actual es: " +str(costo)) #Muestra el costo actual 
+                
+            if(estado_hora2 == '1'):#Si el comedero para mascotas a las 12pm se encuentra vacio
+                print("El comedero para mascotas a las 12pm esta vacio")#Muestra mensaje de que el comedero para mascotas esta vacio a las 12pm
+                print("El comedero de mascotas avanza a las 12pm") 
+                costo += 1 #Incrementa el costo por avanzar a las 12pm
+                print("El costo actual es: " +str(costo)) #Muestra el costo actual
+                estado_meta['12pm'] ='0'#El comedero para mascotas a la hora 12pm se llenara
+                costo += 1 #El costo aumentara cada vez que el comedero cambie de estado, es decir de estar vacio pase a estar lleno
+                print("El comedero para mascotas se esta llenando a las 12pm")#Muestra que el comedero para mascotas se esta llenando a esa hora
+                print("El costo actual es: " +str(costo)) #Muestra el costo actual
+            else:
+                print("El comedero para mascotas a las 12pm esta lleno")#Cuando el comedero para mascotas se encuentra lleno
+                print("Ninguna acción. El costo actual es: "  +str(costo))#El costo se mantiene
+                
+            if(estado_hora3 == '1'):#Si el comedero para mascotas a las 7pm se encuentra vacio
+                print("El comedero para mascotas a las 12pm esta vacio")
+                print("EEl comedero de mascotas avanza a las 7pm") 
+                costo += 1 #Incrementa el costo dado que el comedero para mascotas avanza
+                print("El costo actual es: " +str(costo)) #Muestra el costo actual
+                estado_meta['7pm'] ='0'
+                costo += 1 #El costo aumentara cada que el comedero para mascotas se llene
+                print("El comedero para mascotas se esta llenando a las 7pm")#Muestra que el comedero para mascotas se esta llenando a esa hora
+                print("El costo actual es: "  +str(costo)) #Muestra el costo actual 
+            else:
+                print("El comedero para mascotas a las 12pm esta lleno")#Cuando el comedero para mascotas se encuentra lleno
+                print("Ninguna acción. El costo actual es: "  +str(costo))#El costo se mantiene
+                
